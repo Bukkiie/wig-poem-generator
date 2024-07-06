6bcfca5d14c916e8c068208f784264e5ff3a6487
Bukkiie <adebukolaabimbolaajayi@gmail.com>
2024-07-06 18:48:03 +0000

    Added AI API intregration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
+function displayPoem (response) {
+     new Typewriter("#poem", {
+       strings: response.data.answer,
+       autoStart: true,
+       delay: 1,
+       cursor: null,
+     });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: ["A wig that makes my style transcend"],
-    autoStart: true,
-    delay: 1,
-    cursor: null,
-    
-  });
+  let instructionInput = document.querySelector ("#user-instruction");
+  let apiKey = "8c0c4c1tabboa3733f55eb4e7a8c07bf";
+  let prompt = `User Instruction: Generate a catchy poem about ${instructionInput.value}`;
+  let context = "You are a poem expert that loves to write short and catchy poems. Your mission is to generate a 4 line poem and separate each line with a <br/>. Make sure to follow the user instruction";
+  let apiUrl =
+    `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get (apiUrl). then (displayPoem);
+
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
